f655f478ea43b34acfff0eceec2ba257399daef3
Radamés <radames.idiomas@gmail.com>
2024-05-08 23:04:38 -0300

    criando o template e a view para o resultado

--- calculadora/calculatorapp/views.py
+++ calculadora/calculatorapp/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def resultado(request):
+    numero_1 = int(request.GET.get('numero_1'))
+    numero_2 = int(request.GET.get('numero_2'))
+    
+    if request.GET.get('add') == "":
+        resposta = numero_1 + numero_2
+    elif request.GET.get('subtract') == "":
+        resposta = numero_1 - numero_2 
+    elif request.GET.get('multiply') == "":
+        resposta = numero_1 * numero_2
+    else:
+        resposta = numero_1 / numero_2
+        
+    return render(request, 'resultado.html', {'resposta': resposta})
+        
